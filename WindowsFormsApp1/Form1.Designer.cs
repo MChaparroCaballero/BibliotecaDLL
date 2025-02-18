592e36e31a50297570086062db6cf50413f3cf91
María <mchaparrocaballero@gmail.com>
2025-02-18 21:41:14 +0100

    fast,play,carpeta,progress,cerrar

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonBefore1 = new ELEMENTOS.ButtonBefore();
+            this.buttonCerrar1 = new ELEMENTOS.ButtonCerrar();
+            this.buttonCarpeta2 = new ELEMENTOS.ButtonCarpeta();
+            this.buttonPlay2 = new ELEMENTOS.ButtonPlay();
+            this.buttonAceptar1 = new ELEMENTOS.ButtonAceptar();
+            this.progressBarButton1 = new ELEMENTOS.ProgressBarButton();
+            this.buttonBefore2 = new ELEMENTOS.ButtonBefore();
+            this.SuspendLayout();
+            // 
+            // buttonBefore1
+            // 
+            this.buttonBefore1.BackColor = System.Drawing.Color.DarkRed;
+            this.buttonBefore1.BackgroundColor = System.Drawing.Color.DarkRed;
+            this.buttonBefore1.BorderColor = System.Drawing.Color.DarkRed;
+            this.buttonBefore1.BorderRadius = 40;
+            this.buttonBefore1.BorderSize = 0;
+            this.buttonBefore1.FlatAppearance.BorderSize = 0;
+            this.buttonBefore1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonBefore1.ForeColor = System.Drawing.Color.DarkRed;
+            this.buttonBefore1.Location = new System.Drawing.Point(424, 266);
+            this.buttonBefore1.Name = "buttonBefore1";
+            this.buttonBefore1.RotationAngle = 0F;
+            this.buttonBefore1.Size = new System.Drawing.Size(40, 40);
+            this.buttonBefore1.TabIndex = 8;
+            this.buttonBefore1.Text = "buttonBefore1";
+            this.buttonBefore1.TextColor = System.Drawing.Color.DarkRed;
+            this.buttonBefore1.UseVisualStyleBackColor = false;
+            // 
+            // buttonCerrar1
+            // 
+            this.buttonCerrar1.BackColor = System.Drawing.Color.DarkRed;
+            this.buttonCerrar1.BackgroundColor = System.Drawing.Color.DarkRed;
+            this.buttonCerrar1.BorderColor = System.Drawing.Color.DarkRed;
+            this.buttonCerrar1.BorderRadius = 40;
+            this.buttonCerrar1.BorderSize = 0;
+            this.buttonCerrar1.FlatAppearance.BorderSize = 0;
+            this.buttonCerrar1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCerrar1.ForeColor = System.Drawing.Color.DarkRed;
+            this.buttonCerrar1.Location = new System.Drawing.Point(724, 12);
+            this.buttonCerrar1.Name = "buttonCerrar1";
+            this.buttonCerrar1.Size = new System.Drawing.Size(40, 40);
+            this.buttonCerrar1.TabIndex = 6;
+            this.buttonCerrar1.Text = "buttonCerrar1";
+            this.buttonCerrar1.TextColor = System.Drawing.Color.DarkRed;
+            this.buttonCerrar1.UseVisualStyleBackColor = false;
+            // 
+            // buttonCarpeta2
+            // 
+            this.buttonCarpeta2.BackColor = System.Drawing.Color.DarkRed;
+            this.buttonCarpeta2.BackgroundColor = System.Drawing.Color.DarkRed;
+            this.buttonCarpeta2.BorderColor = System.Drawing.Color.DarkRed;
+            this.buttonCarpeta2.BorderRadius = 40;
+            this.buttonCarpeta2.BorderSize = 0;
+            this.buttonCarpeta2.FlatAppearance.BorderSize = 0;
+            this.buttonCarpeta2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCarpeta2.ForeColor = System.Drawing.Color.DarkRed;
+            this.buttonCarpeta2.Location = new System.Drawing.Point(21, 12);
+            this.buttonCarpeta2.Name = "buttonCarpeta2";
+            this.buttonCarpeta2.Size = new System.Drawing.Size(40, 40);
+            this.buttonCarpeta2.TabIndex = 5;
+            this.buttonCarpeta2.Text = "buttonCarpeta2";
+            this.buttonCarpeta2.TextColor = System.Drawing.Color.DarkRed;
+            this.buttonCarpeta2.UseVisualStyleBackColor = false;
+            // 
+            // buttonPlay2
+            // 
+            this.buttonPlay2.BackColor = System.Drawing.Color.DarkRed;
+            this.buttonPlay2.BackgroundColor = System.Drawing.Color.DarkRed;
+            this.buttonPlay2.BorderColor = System.Drawing.Color.DarkRed;
+            this.buttonPlay2.BorderRadius = 40;
+            this.buttonPlay2.BorderSize = 0;
+            this.buttonPlay2.FlatAppearance.BorderSize = 0;
+            this.buttonPlay2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPlay2.ForeColor = System.Drawing.Color.DarkRed;
+            this.buttonPlay2.Location = new System.Drawing.Point(360, 266);
+            this.buttonPlay2.Name = "buttonPlay2";
+            this.buttonPlay2.Size = new System.Drawing.Size(40, 40);
+            this.buttonPlay2.TabIndex = 3;
+            this.buttonPlay2.Text = "buttonPlay2";
+            this.buttonPlay2.TextColor = System.Drawing.Color.DarkRed;
+            this.buttonPlay2.UseVisualStyleBackColor = false;
+            // 
+            // buttonAceptar1
+            // 
+            this.buttonAceptar1.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.buttonAceptar1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.buttonAceptar1.ForeColor = System.Drawing.Color.White;
+            this.buttonAceptar1.Location = new System.Drawing.Point(674, 370);
+            this.buttonAceptar1.Name = "buttonAceptar1";
+            this.buttonAceptar1.Size = new System.Drawing.Size(100, 50);
+            this.buttonAceptar1.TabIndex = 0;
+            this.buttonAceptar1.Text = "buttonAceptar1";
+            this.buttonAceptar1.UseVisualStyleBackColor = false;
+            // 
+            // progressBarButton1
+            // 
+            this.progressBarButton1.ChannelColor = System.Drawing.Color.LightSteelBlue;
+            this.progressBarButton1.ChannelHeight = 6;
+            this.progressBarButton1.ForeBackColor = System.Drawing.Color.RoyalBlue;
+            this.progressBarButton1.ForeColor = System.Drawing.Color.White;
+            this.progressBarButton1.Location = new System.Drawing.Point(255, 223);
+            this.progressBarButton1.Name = "progressBarButton1";
+            this.progressBarButton1.ShowMaximun = false;
+            this.progressBarButton1.ShowValue = ELEMENTOS.TextPosition.Right;
+            this.progressBarButton1.Size = new System.Drawing.Size(238, 23);
+            this.progressBarButton1.SliderColor = System.Drawing.Color.RoyalBlue;
+            this.progressBarButton1.SliderHeight = 6;
+            this.progressBarButton1.SymbolAfter = "";
+            this.progressBarButton1.SymbolBefore = "";
+            this.progressBarButton1.TabIndex = 10;
+            // 
+            // buttonBefore2
+            // 
+            this.buttonBefore2.BackColor = System.Drawing.Color.DarkRed;
+            this.buttonBefore2.BackgroundColor = System.Drawing.Color.DarkRed;
+            this.buttonBefore2.BorderColor = System.Drawing.Color.DarkRed;
+            this.buttonBefore2.BorderRadius = 40;
+            this.buttonBefore2.BorderSize = 0;
+            this.buttonBefore2.FlatAppearance.BorderSize = 0;
+            this.buttonBefore2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonBefore2.ForeColor = System.Drawing.Color.DarkRed;
+            this.buttonBefore2.Location = new System.Drawing.Point(295, 266);
+            this.buttonBefore2.Name = "buttonBefore2";
+            this.buttonBefore2.RotationAngle = 0F;
+            this.buttonBefore2.Size = new System.Drawing.Size(40, 40);
+            this.buttonBefore2.TabIndex = 11;
+            this.buttonBefore2.Text = "buttonBefore2";
+            this.buttonBefore2.TextColor = System.Drawing.Color.DarkRed;
+            this.buttonBefore2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonBefore2);
+            this.Controls.Add(this.progressBarButton1);
+            this.Controls.Add(this.buttonBefore1);
+            this.Controls.Add(this.buttonCerrar1);
+            this.Controls.Add(this.buttonCarpeta2);
+            this.Controls.Add(this.buttonPlay2);
+            this.Controls.Add(this.buttonAceptar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ELEMENTOS.ButtonAceptar buttonAceptar1;
+        private ELEMENTOS.ButtonPlay buttonPlay2;
+        private ELEMENTOS.ButtonCarpeta buttonCarpeta2;
+        private ELEMENTOS.ButtonCerrar buttonCerrar1;
+        private ELEMENTOS.ButtonBefore buttonBefore1;
+        private ELEMENTOS.ProgressBarButton progressBarButton1;
+        private ELEMENTOS.ButtonBefore buttonBefore2;
+    }
+}
+
